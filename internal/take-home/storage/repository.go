4b208dea6419cf5ee77feb03c4e847ca3197902a
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2025-04-14 17:46:49 +0200

    STORAGE: add repository.go

---
+++ internal/take-home/storage/repository.go
@@ -0,0 +1,17 @@
+package storage
+
+import (
+	"context"
+	"time"
+
+	"github.com/francescorizzello94/senior-fullstack-engineer-takehome/internal/take-home/model"
+)
+
+// interface defines the behaviour in relation to the database
+
+type WeatherRepository interface {
+	InsertWeatherData(ctx context.Context, data any) error
+	GetByDate(ctx context.Context, date time.Time) ([]*model.WeatherData, error)
+	GetByDateRange(ctx context.Context, start, end time.Time) ([]*model.WeatherData, error)
+	CloseConnection(ctx context.Context) error
+}
