6f016c0c97fb2b8385c6e38568898732fcc0f536
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2025-04-14 17:08:41 +0200

    SERVICE: add ingest.go

--- internal/take-home/service/ingest.go
+++ internal/take-home/service/ingest.go
@@ -1,1 +1,44 @@
 package service
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/francescorizzello94/senior-fullstack-engineer-takehome/internal/take-home/model"
+	"github.com/francescorizzello94/senior-fullstack-engineer-takehome/internal/take-home/storage"
+)
+
+type IngestService struct {
+	repo   storage.MongoDBRepository
+	parser *WeatherParser
+}
+
+func NewIngestService(repo storage.MongoDBRepository) *IngestService {
+	return &IngestService{
+		repo:   repo,
+		parser: NewWeatherParser(),
+	}
+}
+
+func (s *IngestService) IngestFile(ctx context.Context, filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	return s.parser.ParseStream(ctx, file, func(data *model.WeatherData) error {
+		if err := s.repo.InsertWeatherData(ctx, data); err != nil {
+			return fmt.Errorf("failed to insert data: %w", err)
+		}
+		return nil
+	})
+}
+
+func (s *IngestService) IngestSingle(ctx context.Context, data *model.WeatherData) error {
+	if err := data.Validate(); err != nil {
+		return fmt.Errorf("invalid data: %w", err)
+	}
+	return s.repo.InsertWeatherData(ctx, data)
+}
