d859102a2f44dd73bcc0b5179390b0948be816e7
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2025-04-15 11:14:06 +0200

    STORAGE: update repository.go to use flexible opts

--- internal/take-home/storage/repository.go
+++ internal/take-home/storage/repository.go
@@ -11,7 +11,7 @@ import (
 
 type WeatherRepository interface {
 	InsertWeatherData(ctx context.Context, data any) error
-	GetByDate(ctx context.Context, date time.Time, opts *QueryOptions) ([]*model.WeatherData, error)
-	GetByDateRange(ctx context.Context, start, end time.Time, opts *QueryOptions) ([]*model.WeatherData, error)
+	GetByDate(ctx context.Context, date time.Time, opts ...*QueryOptions) ([]*model.WeatherData, error)
+	GetByDateRange(ctx context.Context, start, end time.Time, opts ...*QueryOptions) ([]*model.WeatherData, error)
 	CloseConnection(ctx context.Context) error
 }
