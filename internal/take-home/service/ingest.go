3f8e1c22f5c03ed51fb2cd742f033dbf734497e0
Francesco Pietro Rizzello <francescorizzello94@gmail.com>
2025-04-16 00:40:11 +0200

    SERVICE: update ingest.go to implement interface pattern

--- internal/take-home/service/ingest.go
+++ internal/take-home/service/ingest.go
@@ -4,17 +4,28 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/francescorizzello94/senior-fullstack-engineer-takehome/internal/take-home/model"
 	"github.com/francescorizzello94/senior-fullstack-engineer-takehome/internal/take-home/storage"
 )
+
+type IngestServiceInterface interface {
+	IngestFile(ctx context.Context, filePath string) error
+	IngestSingle(ctx context.Context, data *model.WeatherData) error
+}
+
+type QueryServiceInterface interface {
+	GetByDate(ctx context.Context, date time.Time, opts ...*QueryOptions) ([]*model.WeatherData, error)
+	GetByDateRange(ctx context.Context, start, end time.Time, opts ...*QueryOptions) ([]*model.WeatherData, error)
+}
 
 type IngestService struct {
 	repo   storage.WeatherRepository
 	parser *WeatherParser
 }
 
-func NewIngestService(repo storage.WeatherRepository) *IngestService {
+func NewIngestService(repo storage.WeatherRepository) IngestServiceInterface {
 	return &IngestService{
 		repo:   repo,
 		parser: NewWeatherParser(),
@@ -40,5 +51,13 @@ func (s *IngestService) IngestSingle(ctx context.Context, data *model.WeatherDat
 	if err := data.Validate(); err != nil {
 		return fmt.Errorf("invalid data: %w", err)
 	}
+
+	data.Date = time.Date(
+		data.Date.Year(),
+		data.Date.Month(),
+		data.Date.Day(),
+		0, 0, 0, 0,
+		time.UTC,
+	)
 	return s.repo.InsertWeatherData(ctx, data)
 }
